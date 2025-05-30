7b65aa807a42ed0450fb61937e98b935b7af43bc
kurorinchan <kurorinchan@users.noreply.github.com>
2025-05-30 17:26:59 +0900

    Fix typo in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/time-recorder-app-react/',
+  base: '/time-record-app-react/',
 })
